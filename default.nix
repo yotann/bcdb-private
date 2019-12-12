404048fa402ba0f62835017e86fa3b13243e3312
Sean Bartell <smbarte2@illinois.edu>
2019-12-12 13:04:14 -0600

    add support for llvm 10.0.0git, used by Alive2

--- default.nix
+++ default.nix
@@ -36,6 +36,9 @@ rec {
   bcdb-llvm9 = callPackage ./build.nix {
     llvm = debugLLVM llvmPackages_9;
   };
+  bcdb-llvmAlive = callPackage ./build.nix {
+    llvm = llvmForAlive;
+  };
 
   bcdb-clang = callPackage ./build.nix {
     inherit (llvmPackages_7) stdenv;
@@ -43,4 +46,27 @@ rec {
   };
 
   bcdb = bcdb-llvm7;
+
+  llvmForAlive = (llvmPackages_9.llvm.override {
+    debugVersion = true;
+    enableSharedLibraries = true;
+  }).overrideAttrs (o: {
+    cmakeFlags = o.cmakeFlags ++ [
+      "-DLLVM_ENABLE_RTTI=ON"
+      "-DLLVM_ENABLE_EH=ON"
+    ];
+    doCheck = false;
+    src = fetchFromGitHub {
+      owner = "llvm";
+      repo = "llvm-project";
+      rev = "22d516261a98fd56ccce39b3031fdba8d64de696";
+      sha256 = "1n63k7dq4rgcp754vjiyrpm41w3z4gdnf0ssa80q9i0rpc5fhfxq";
+    };
+    unpackPhase = null;
+    sourceRoot = "source/llvm";
+    postPatch = ''
+      substitute '${./nix/llvm-outputs.patch}' ./llvm-outputs.patch --subst-var lib
+      patch -p1 < ./llvm-outputs.patch
+    '';
+  });
 }
